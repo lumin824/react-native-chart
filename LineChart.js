d4a310e7d7b6433803a0a39bb7864ad1e17faf72
lumin <lumin824@163.com>
2016-06-15 19:40:21 +0800

    init

---
+++ LineChart.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import {
+  Text,
+  View
+} from 'react-native';
+
+export default class LineChart extends Component {
+  render(){
+    return (
+      <View style={{height:30}}>
+        <Text>LineChart</Text>
+      </View>
+    );
+  }
+}
