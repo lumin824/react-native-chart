ed6c20428be1b59a8ad47db2d255a2df8730a2ac
lumin <lumin824@163.com>
2016-06-20 08:48:27 +0800

    ios chart swipe direction

--- ios/RNWarpView.m
+++ ios/RNWarpView.m
@@ -9,20 +9,41 @@
 {
     self = [super init];
     if(self){
-        [subview setFrame:CGRectMake(0, 0, 100, 100)];
         [self addSubview:subview];
         
-        UISwipeGestureRecognizer* swipeGestureRecognizer = [[UISwipeGestureRecognizer alloc] initWithTarget:self action:@selector(onChartGestureHandle:)];
+        UISwipeGestureRecognizer* swipeGestureRecognizer = nil;
         
+        swipeGestureRecognizer = [[UISwipeGestureRecognizer alloc] initWithTarget:self action:@selector(onSwipeGestureHandle:)];
+        swipeGestureRecognizer.direction = UISwipeGestureRecognizerDirectionRight;
         [self addGestureRecognizer:swipeGestureRecognizer];
+        
+        swipeGestureRecognizer = [[UISwipeGestureRecognizer alloc] initWithTarget:self action:@selector(onSwipeGestureHandle:)];
+        swipeGestureRecognizer.direction = UISwipeGestureRecognizerDirectionLeft;
+        [self addGestureRecognizer:swipeGestureRecognizer];
+        
+        swipeGestureRecognizer = [[UISwipeGestureRecognizer alloc] initWithTarget:self action:@selector(onSwipeGestureHandle:)];
+        swipeGestureRecognizer.direction = UISwipeGestureRecognizerDirectionUp;
+        [self addGestureRecognizer:swipeGestureRecognizer];
+        
+        swipeGestureRecognizer = [[UISwipeGestureRecognizer alloc] initWithTarget:self action:@selector(onSwipeGestureHandle:)];
+        swipeGestureRecognizer.direction = UISwipeGestureRecognizerDirectionDown;
+        [self addGestureRecognizer:swipeGestureRecognizer];
+        
     }
     return self;
 }
 
--(void)onChartGestureHandle:(UISwipeGestureRecognizer*)swipeGestureRecognizer
+-(void)onSwipeGestureHandle:(UISwipeGestureRecognizer*)swipeGestureRecognizer
 {
+    NSString* direction = @"";
     if(self.onChartGesture){
-        self.onChartGesture(@{});
+        switch(swipeGestureRecognizer.direction){
+            case UISwipeGestureRecognizerDirectionRight: direction = @"right"; break;
+            case UISwipeGestureRecognizerDirectionLeft: direction = @"left"; break;
+            case UISwipeGestureRecognizerDirectionUp: direction = @"up"; break;
+            case UISwipeGestureRecognizerDirectionDown: direction = @"down"; break;
+        }
+        self.onChartGesture(@{@"direction":direction});
     }
 }
 
